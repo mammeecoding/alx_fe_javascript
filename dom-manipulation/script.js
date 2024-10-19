4196d01661da015312cdf337a90fa722200833d5
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-10-19 21:00:25 +0300

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -3,18 +3,18 @@ const newQuote = document.getElementById("newQuote");
 const showRandomQuote = [
   {
     text: "I think there are going to be a bunch of tablet-like devices. its a really different product catagory",
-    catagory: "Jeff Bezos"
+    category: "Jeff Bezos"
   },
   {
     text: "inanimate objects can be classified scientifically into three major catagories; those that don't work.those that break down and those that get lost",
-    catagory: "Russell Baker"
+    category: "Russell Baker"
   },
   {
     text: "I think there are going to be a bunch of tablet-like devices. its a really different product catagory",
-    catagory: "Jeff Bezos"
+    category: "Jeff Bezos"
   },
   {
     text: "inanimate objects can be classified scientifically into three major catagories; those that don't work.those that break down and those that get lost",
-    catagory: "Russell Baker"
+    category: "Russell Baker"
   }
 ];
